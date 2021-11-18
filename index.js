cd2dbc37d2c52138d3cba92e8875bde70b0f84fb
oussama-belm <oo1019848@gmail.com>
2021-11-18 21:33:08 +0100

    new website

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var body = document.querySelector('body');
+    function fired(){
+        console.log('the function fired try again next time harder !!');
+    }
+    var 
+    function hoverd(){
+        console.log('hoverd ');
+    }
+    logo.addEventListener('click',fired);
+    logo.addEventListener('onclick',hoverd)
+})
